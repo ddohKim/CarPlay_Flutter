41d30611270f36f121c3c493315bfff5b5f6be10
DoHyunKim <95413669+ddohKim@users.noreply.github.com>
2024-10-02 12:31:45 +0900

    Add iOS Build File

---
+++ ios/Pods/Target Support Files/flutter_carplay/flutter_carplay-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_flutter_carplay : NSObject
+@end
+@implementation PodsDummy_flutter_carplay
+@end
---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<flutter_carplay/FlutterCarplayPlugin.h>)
+#import <flutter_carplay/FlutterCarplayPlugin.h>
+#else
+@import flutter_carplay;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FlutterCarplayPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterCarplayPlugin"]];
+}
+
+@end
